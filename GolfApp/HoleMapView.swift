4afcba42c518f69e30f0c91c6a3acab0290ca9b9
Jonathan Linder (M2 mac) <jonathan.linder08@icloud.com>
2024-06-19 20:36:34 +0200

    test

---
+++ GolfApp/HoleMapView.swift
@@ -0,0 +1,18 @@
+//
+//  HoleMapView.swift
+//  GolfApp
+//
+//  Created by Jonathan Linder on 2024-06-19.
+//
+
+import SwiftUI
+
+struct HoleMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HoleMapView()
+}
---
+++ GolfApp/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  GolfApp
+//
+//  Created by Jonathan Linder on 2024-06-19.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
