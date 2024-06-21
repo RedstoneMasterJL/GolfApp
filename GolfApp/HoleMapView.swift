a699db81822ea8878f05f064166f61eb2435bf0f
Jonathan Linder (M2 mac) <jonathan.linder08@icloud.com>
2024-06-21 18:17:06 +0200

    verkar funka med draggable pin från https://www.youtube.com/watch?v=L2_JRCG6LQo

--- GolfApp/HoleMapView.swift
+++ GolfApp/HoleMapView.swift
@@ -13,27 +13,83 @@ struct HoleMapView: View {
     @Binding var hole: Hole
     let modes: MapInteractionModes = [.zoom, .pan, .pitch]
     
+    @State var coordinate: CLLocationCoordinate2D?
     var body: some View {
-        Map(position: Binding<MapCameraPosition>(get: {
-            .camera(MapCamera(MKMapCamera(lookingAtCenter: hole.centerPos, fromEyeCoordinate: hole.teePos, eyeAltitude: hole.teeGreenDistance * 2.5)))
-        }, set: { newValue in
+        MapReader { proxy in
+            Map(position: Binding<MapCameraPosition>(get: {
+                .camera(MapCamera(MKMapCamera(lookingAtCenter: hole.centerPos, fromEyeCoordinate: hole.teePos, eyeAltitude: hole.teeGreenDistance * 2.5)))
+            }, set: { newValue in
+                
+            }), interactionModes: modes) {
+                UserAnnotation()
+                Annotation("", coordinate: hole.greenPos) {
+                    Circle().fill(.green)
+                }
+                Annotation("", coordinate: coordinate ?? hole.centerPos) {
+                    DraggablePin(proxy: proxy, coordinate: $coordinate.withDefault(hole.centerPos))
+                }
+                Annotation("", coordinate: hole.teePos) {
+                    Circle().fill(.yellow)
+                }
+            }.mapStyle(.hybrid(elevation: .realistic))
+                .mapControls { MapScaleView() }
+        }
+    }
+}
+
+struct DraggablePin: View {
+    var proxy: MapProxy
+    @Binding var coordinate: CLLocationCoordinate2D
+    @State private var isActive = false
+    @State private var translation: CGSize = .zero
+    
+    var body: some View {
+        GeometryReader {
+            let frame = $0.frame(in: .global)
             
-        }), interactionModes: modes) {
-            UserAnnotation()
-            Annotation("", coordinate: hole.greenPos) {
-                Circle().fill(.green)
-            }
-//            Annotation("", coordinate: hole.centerPos) {
-//                Image(systemName: "scope").foregroundStyle(.blue)
-//            }
-            Annotation("", coordinate: hole.teePos) {
-                Circle().fill(.yellow)
-            }
-        }.mapStyle(.hybrid(elevation: .realistic))
-            .mapControls { MapScaleView() }
+            Image(systemName: "mappin")
+                .font(.largeTitle)
+                .foregroundStyle(.orange.gradient)
+                .animation(.snappy, body: { content in
+                    content.scaleEffect(isActive ? 1.3 : 1)
+                })
+                .frame(width: frame.width, height: frame.height)
+                .onChange(of: isActive) { oldValue, newValue in
+                    let position = CGPoint(x: frame.midX, y: frame.midY)
+                    if let coordinate = proxy.convert(position, from: .global) {
+                        self.coordinate = coordinate
+                        translation = .zero
+                    }
+                }
+        }
+        .frame(width: 30, height: 30)
+        .contentShape(.rect)
+        .offset(translation)
+        .gesture(
+            LongPressGesture(minimumDuration: 0.15)
+                .onEnded{ isActive = $0 }
+                .simultaneously(with: DragGesture(minimumDistance: 0)
+                    .onChanged { value in
+                        if isActive { translation = value.translation }
+                    }
+                    .onEnded { value in
+                        if isActive { isActive = false }
+                    }
+                )
+        )
     }
 }
 
 #Preview {
     HoleMapView(hole: .constant(holes[0]))
+}
+
+extension Binding {
+  func withDefault<T>(_ defaultValue: T) -> Binding<T> where Value == Optional<T> {
+    return Binding<T>(get: {
+      self.wrappedValue ?? defaultValue
+    }, set: { newValue in
+      self.wrappedValue = newValue
+    })
+  }
 }
