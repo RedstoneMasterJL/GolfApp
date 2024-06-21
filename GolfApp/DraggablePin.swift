cd8574fd079cb8a7b9d4a699e389124fc06da69b
Jonathan Linder (M2 mac) <jonathan.linder08@icloud.com>
2024-06-21 21:14:04 +0200

    draggable pin separat fil

--- GolfApp/DraggablePin.swift
+++ GolfApp/DraggablePin.swift
@@ -5,4 +5,49 @@
 //  Created by Jonathan Linder on 2024-06-21.
 //
 
-import Foundation
+import SwiftUI
+import MapKit
+
+struct DraggablePin: View {
+    var proxy: MapProxy
+    @Binding var coordinate: CLLocationCoordinate2D
+    @State private var isActive = false
+    @State private var translation: CGSize = .zero
+    
+    var body: some View {
+        GeometryReader {
+            let frame = $0.frame(in: .global)
+            
+            Image(systemName: "dot.scope")
+                .font(.system(size: 50))
+                .fontWeight(.thin)
+                .foregroundStyle(.orange.gradient)
+                .animation(.snappy, body: { content in
+                    content.scaleEffect(isActive ? 1.3 : 1)
+                })
+                .frame(width: frame.width, height: frame.height)
+                .onChange(of: isActive, initial: false) { oldValue, newValue in
+                    let position = CGPoint(x: frame.midX, y: frame.midY)
+                    if let coordinate = proxy.convert(position, from: .global) {
+                        self.coordinate = coordinate
+                        translation = .zero
+                    }
+                }
+        }
+        .frame(width: 70, height: 70)
+        .contentShape(.rect)
+        .offset(translation)
+        .gesture(
+            LongPressGesture(minimumDuration: 0)
+                .onEnded { isActive = $0 }
+                .simultaneously(with: DragGesture(minimumDistance: 0)
+                    .onChanged { value in
+                        if isActive { translation = value.translation }
+                    }
+                    .onEnded { value in
+                        if isActive { isActive = false }
+                    }
+                )
+        )
+    }
+}
--- GolfApp/HoleMapView.swift
+++ GolfApp/HoleMapView.swift
@@ -36,50 +36,6 @@ struct HoleMapView: View {
     }
 }
 
-struct DraggablePin: View {
-    var proxy: MapProxy
-    @Binding var coordinate: CLLocationCoordinate2D
-    @State private var isActive = false
-    @State private var translation: CGSize = .zero
-    
-    var body: some View {
-        GeometryReader {
-            let frame = $0.frame(in: .global)
-            
-            Image(systemName: "dot.scope")
-                .font(.system(size: 50))
-                .fontWeight(.thin)
-                .foregroundStyle(.orange.gradient)
-                .animation(.snappy, body: { content in
-                    content.scaleEffect(isActive ? 1.3 : 1)
-                })
-                .frame(width: frame.width, height: frame.height)
-                .onChange(of: isActive, initial: false) { oldValue, newValue in
-                    let position = CGPoint(x: frame.midX, y: frame.midY)
-                    if let coordinate = proxy.convert(position, from: .global) {
-                        self.coordinate = coordinate
-                        translation = .zero
-                    }
-                }
-        }
-        .frame(width: 70, height: 70)
-        .contentShape(.rect)
-        .offset(translation)
-        .gesture(
-            LongPressGesture(minimumDuration: 0)
-                .onEnded { isActive = $0 }
-                .simultaneously(with: DragGesture(minimumDistance: 0)
-                    .onChanged { value in
-                        if isActive { translation = value.translation }
-                    }
-                    .onEnded { value in
-                        if isActive { isActive = false }
-                    }
-                )
-        )
-    }
-}
-
 #Preview {
     HoleMapView(hole: .constant(holes[0]))
 }
