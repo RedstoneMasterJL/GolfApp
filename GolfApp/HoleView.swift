e42f2dffb8e10cca0c576f028418777896b8dbd1
Jonathan Linder (M2 mac) <jonathan.linder08@icloud.com>
2024-06-22 13:58:07 +0200

    lade till HoleView

---
+++ GolfApp/HoleView.swift
@@ -0,0 +1,18 @@
+//
+//  HoleView.swift
+//  GolfApp
+//
+//  Created by Jonathan Linder on 2024-06-22.
+//
+
+import SwiftUI
+
+struct HoleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HoleView()
+}
