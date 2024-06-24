cf61ab095ac35af4b16259c6b92969574976c1a3
Jonathan Linder (M2 mac) <jonathan.linder08@icloud.com>
2024-06-24 18:07:16 +0200

    testar med dragholesview

---
+++ GolfApp/DragHolesView.swift
@@ -0,0 +1,18 @@
+//
+//  DragHolesView.swift
+//  GolfApp
+//
+//  Created by Jonathan Linder on 2024-06-24.
+//
+
+import SwiftUI
+
+struct DragHolesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DragHolesView()
+}
