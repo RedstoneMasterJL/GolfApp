9462187e5389e932f54e2ef0355748a7616ec362
Jonathan Linder (M2 mac) <jonathan.linder08@icloud.com>
2024-06-25 21:44:30 +0200

    gjort cllocationcoordinate2d till codable, mapcameraposition är svårare

--- GolfApp/Hole.swift
+++ GolfApp/Hole.swift
@@ -7,8 +7,32 @@
 
 import SwiftUI
 import MapKit
+import SwiftData
 
-@Observable
+extension CLLocationCoordinate2D: Codable {
+    enum CodingKeys: String, CodingKey {
+        case latitude
+        case longitude
+    }
+    
+    // Initialize from decoder
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let latitude = try container.decode(CLLocationDegrees.self, forKey: .latitude)
+        let longitude = try container.decode(CLLocationDegrees.self, forKey: .longitude)
+        self.init(latitude: latitude, longitude: longitude)
+    }
+    
+    // Encode to encoder
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(latitude, forKey: .latitude)
+        try container.encode(longitude, forKey: .longitude)
+    }
+}
+
+
+@Model
 class HoleData {
 
     let num: Int
