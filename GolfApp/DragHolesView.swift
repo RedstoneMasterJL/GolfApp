f7a0a62118139013138ced03e178e891c326decd
Jonathan Linder (M2 mac) <jonathan.linder08@icloud.com>
2024-06-25 11:33:33 +0200

    vekrar funka bra, ändrade till index istället på foreach loopen och uppdaterar mycket bättre än @Bindable i view bodyn som knappt uppdaterade, dessutom gjorde jag  translation och isactive privat i Markerview samt lade greenMarkerPos & teeMarkerPos i HoleMarkerData istället för separata klasser

--- GolfApp/DragHolesView.swift
+++ GolfApp/DragHolesView.swift
@@ -21,8 +21,8 @@ struct DragHolesView: View {
 
     init() {
         markers = [
-            //HoleMarkerData(holeNum: 1, greenMarker: GreenMarker(coordinate: CLLocationCoordinate2D(latitude: 57.78191, longitude: 11.95473)),teeMarker: TeeMarker(coordinate: CLLocationCoordinate2D(latitude: 57.78120, longitude: 11.95568)))
-            HoleMarkerData(holeNum: 1, greenPos: CLLocationCoordinate2D(latitude: 57.78191, longitude: 11.95473), teePos: CLLocationCoordinate2D(latitude: 57.78120, longitude: 11.95568))
+            //HoleMarkerData(holeNum: 1, greenPos: CLLocationCoordinate2D(latitude: 57.78191, longitude: 11.95473), teePos: CLLocationCoordinate2D(latitude: 57.78120, longitude: 11.95568))
+            HoleMarkerData(holeNum: 1, greenMarkerPos: CLLocationCoordinate2D(latitude: 57.78191, longitude: 11.95473), teeMarkerPos: CLLocationCoordinate2D(latitude: 57.78120, longitude: 11.95568))
         ]
     }
     
@@ -30,15 +30,33 @@ struct DragHolesView: View {
         MapReader { proxy in
             Map(position: $position) {
                 
-                ForEach(markers) { marker in
-                    @Bindable var marker = marker
-                    Annotation(String(marker.holeNum), coordinate: coordinate) {
-                        MarkerView(proxy: proxy, type: marker.greenMarker.type, coordinate: $coordinate)
-                        //DraggablePin(proxy: proxy, coordinate: $coordinate)
+//                ForEach(markers) { marker in
+//                    @Bindable var marker = marker
+//                    Annotation(String(marker.holeNum), coordinate: marker.greenMarkerPos) {
+//                        MarkerView(proxy: proxy, type: .green, coordinate: $marker.greenMarkerPos)
+//                        //DraggablePin(proxy: proxy, coordinate: $coordinate)
+//                    }
+//                    Annotation(String(marker.holeNum), coordinate: marker.teeMarkerPos) {
+//                        MarkerView(proxy: proxy, type: .tee, coordinate: $marker.teeMarkerPos)
+//                    }
+//                }
+                
+                // detta funkar!
+                Annotation(String(markers[0].holeNum), coordinate: markers[0].teeMarkerPos) {
+                    MarkerView(proxy: proxy, type: .tee, coordinate: $markers[0].teeMarkerPos)
+                }
+                
+                // for each med index är bättre, funkar!!!!
+                // https://developer.apple.com/documentation/swift/range
+                ForEach(0..<markers.count, id: \.self) { i in
+                    Annotation(String(markers[i].holeNum), coordinate: markers[i].greenMarkerPos) {
+                        MarkerView(proxy: proxy, type: .green, coordinate: $markers[i].greenMarkerPos)
                     }
-                    Annotation(String(marker.holeNum), coordinate: marker.teeMarker.coordinate) {
-                        MarkerView(proxy: proxy, type: marker.teeMarker.type, coordinate: $marker.teeMarker.coordinate)
+                    Annotation(String(markers[i].holeNum), coordinate: markers[i].teeMarkerPos) {
+                        MarkerView(proxy: proxy, type: .tee, coordinate: $markers[i].teeMarkerPos)
                     }
+                    MapPolyline(coordinates: [markers[i].greenMarkerPos, markers[i].teeMarkerPos])
+                        .stroke(.orange, lineWidth: 2)
                 }
                 
             }.mapStyle(.imagery(elevation: .realistic))
@@ -51,14 +69,19 @@ struct DragHolesView: View {
                         Button("+") {
                             //markers.append(HoleMarkerData(holeNum: 2, greenPos: proxy.convert(.init(x: rectWidth/4, y: rectWidth/4), from: .global) ?? CLLocationCoordinate2D(latitude: 1, longitude: 1), teePos: proxy.convert(.init(x: rectWidth/4, y: rectWidth/4), from: .global) ?? CLLocationCoordinate2D(latitude: 1, longitude: 1))) // TESTADE HÄR MED SCREENPOINT OCH SÅMT
                             
-                            let newHoleMarker = HoleMarkerData(holeNum: currentHole + 1, greenPos: markers[currentHole-1].greenMarker.coordinate, teePos: markers[currentHole-1].greenMarker.coordinate)
+                            //let newHoleMarker = HoleMarkerData(holeNum: currentHole + 1, greenPos: markers[currentHole-1].greenMarker.coordinate, teePos: markers[currentHole-1].greenMarker.coordinate)
+                            let newHoleMarker = HoleMarkerData(holeNum: currentHole + 1, greenMarkerPos: markers[currentHole-1].greenMarkerPos, teeMarkerPos: markers[currentHole-1].greenMarkerPos)
                             markers.append(newHoleMarker)
                             currentHole += 1
                             // FUNKAR NOG BÄTTRE, sedan kan man även ändra så att den nya markeringen är lite bredvid den andra kanske
                             
                             // VERKAR BLI KONSTIGT I UPPDATERINGEN AV MARKÖRERNA KANSKE NÅGOT MED BINDABLE OCH OBSERVABLE OCH ATT GREENMARKER OCH TEEMARKER ÄR PROPERTYS AV HOLEMARKERDATA, FÅR KOLLA PÅ RElATIONSHIPS OCH SWIFTDATA KANSKE?
                             
-                        }
+                        }.disabled(markers.count == 18)
+                        Button("-") {
+                            markers.removeLast()
+                            currentHole -= 1
+                        }.disabled(markers.count == 1)
                     }.frame(maxWidth: .infinity).padding(.bottom).background(.orange.gradient)
                 }
         }
@@ -81,29 +104,25 @@ enum MarkerType {
 @Observable
 class HoleMarkerData: Identifiable {
     let holeNum: Int
-    var greenMarker: HoleMarker
-    var teeMarker: HoleMarker
+    var greenMarkerPos: CLLocationCoordinate2D
+    var teeMarkerPos: CLLocationCoordinate2D
     
-    init(holeNum: Int, greenPos: CLLocationCoordinate2D, teePos: CLLocationCoordinate2D) {
+    init(holeNum: Int, greenMarkerPos: CLLocationCoordinate2D, teeMarkerPos: CLLocationCoordinate2D) {
         self.holeNum = holeNum
-        self.greenMarker = HoleMarker(type: .green, coordinate: greenPos)
-        self.teeMarker = HoleMarker(type: .tee, coordinate: teePos)
+        self.greenMarkerPos = greenMarkerPos
+        self.teeMarkerPos = teeMarkerPos
     }
 }
 
-@Observable
-class HoleMarker {
-    let type: MarkerType
-    //var translation: CGSize
-    //var isActive: Bool
-    var coordinate: CLLocationCoordinate2D
-    init(type: MarkerType, coordinate: CLLocationCoordinate2D) {
-        self.type = type
-//        self.translation = .zero
-//        self.isActive = false
-        self.coordinate = coordinate
-    }
-}
+///@Observable
+///class HoleMarker {
+///    let type: MarkerType
+///    var coordinate: CLLocationCoordinate2D
+///    init(type: MarkerType, coordinate: CLLocationCoordinate2D) {
+///        self.type = type
+///        self.coordinate = coordinate
+///    }
+///}
 
 
 struct MarkerView: View {
