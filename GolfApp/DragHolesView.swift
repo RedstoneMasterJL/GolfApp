d5ac9f58ca0905ed53bb48ffa206b30704bbadf3
Jonathan Linder (M2 mac) <jonathan.linder08@icloud.com>
2024-06-25 11:42:57 +0200

    tog bort kommentarer från arbetet med DragHolesView i filen

--- GolfApp/DragHolesView.swift
+++ GolfApp/DragHolesView.swift
@@ -21,7 +21,6 @@ struct DragHolesView: View {
 
     init() {
         markers = [
-            //HoleMarkerData(holeNum: 1, greenPos: CLLocationCoordinate2D(latitude: 57.78191, longitude: 11.95473), teePos: CLLocationCoordinate2D(latitude: 57.78120, longitude: 11.95568))
             HoleMarkerData(holeNum: 1, greenMarkerPos: CLLocationCoordinate2D(latitude: 57.78191, longitude: 11.95473), teeMarkerPos: CLLocationCoordinate2D(latitude: 57.78120, longitude: 11.95568))
         ]
     }
@@ -29,25 +28,6 @@ struct DragHolesView: View {
     var body: some View {
         MapReader { proxy in
             Map(position: $position) {
-                
-//                ForEach(markers) { marker in
-//                    @Bindable var marker = marker
-//                    Annotation(String(marker.holeNum), coordinate: marker.greenMarkerPos) {
-//                        MarkerView(proxy: proxy, type: .green, coordinate: $marker.greenMarkerPos)
-//                        //DraggablePin(proxy: proxy, coordinate: $coordinate)
-//                    }
-//                    Annotation(String(marker.holeNum), coordinate: marker.teeMarkerPos) {
-//                        MarkerView(proxy: proxy, type: .tee, coordinate: $marker.teeMarkerPos)
-//                    }
-//                }
-                
-                // detta funkar!
-                Annotation(String(markers[0].holeNum), coordinate: markers[0].teeMarkerPos) {
-                    MarkerView(proxy: proxy, type: .tee, coordinate: $markers[0].teeMarkerPos)
-                }
-                
-                // for each med index är bättre, funkar!!!!
-                // https://developer.apple.com/documentation/swift/range
                 ForEach(0..<markers.count, id: \.self) { i in
                     Annotation(String(markers[i].holeNum), coordinate: markers[i].greenMarkerPos) {
                         MarkerView(proxy: proxy, type: .green, coordinate: $markers[i].greenMarkerPos)
@@ -67,16 +47,9 @@ struct DragHolesView: View {
                         Divider().frame(maxHeight: 15)
                         Text("Nuvarande hål \(currentHole)")
                         Button("+") {
-                            //markers.append(HoleMarkerData(holeNum: 2, greenPos: proxy.convert(.init(x: rectWidth/4, y: rectWidth/4), from: .global) ?? CLLocationCoordinate2D(latitude: 1, longitude: 1), teePos: proxy.convert(.init(x: rectWidth/4, y: rectWidth/4), from: .global) ?? CLLocationCoordinate2D(latitude: 1, longitude: 1))) // TESTADE HÄR MED SCREENPOINT OCH SÅMT
-                            
-                            //let newHoleMarker = HoleMarkerData(holeNum: currentHole + 1, greenPos: markers[currentHole-1].greenMarker.coordinate, teePos: markers[currentHole-1].greenMarker.coordinate)
                             let newHoleMarker = HoleMarkerData(holeNum: currentHole + 1, greenMarkerPos: markers[currentHole-1].greenMarkerPos, teeMarkerPos: markers[currentHole-1].greenMarkerPos)
                             markers.append(newHoleMarker)
                             currentHole += 1
-                            // FUNKAR NOG BÄTTRE, sedan kan man även ändra så att den nya markeringen är lite bredvid den andra kanske
-                            
-                            // VERKAR BLI KONSTIGT I UPPDATERINGEN AV MARKÖRERNA KANSKE NÅGOT MED BINDABLE OCH OBSERVABLE OCH ATT GREENMARKER OCH TEEMARKER ÄR PROPERTYS AV HOLEMARKERDATA, FÅR KOLLA PÅ RElATIONSHIPS OCH SWIFTDATA KANSKE?
-                            
                         }.disabled(markers.count == 18)
                         Button("-") {
                             markers.removeLast()
@@ -114,20 +87,9 @@ class HoleMarkerData: Identifiable {
     }
 }
 
-///@Observable
-///class HoleMarker {
-///    let type: MarkerType
-///    var coordinate: CLLocationCoordinate2D
-///    init(type: MarkerType, coordinate: CLLocationCoordinate2D) {
-///        self.type = type
-///        self.coordinate = coordinate
-///    }
-///}
-
 
 struct MarkerView: View {
     var proxy: MapProxy
-    //@Binding var marker: HoleMarker
     
     let type: MarkerType
     @State private var translation: CGSize = .zero
@@ -169,68 +131,3 @@ struct MarkerView: View {
         )
     }
 }
-
-/*
- // Green
- Annotation("1", coordinate: coordinate) {
-     GeometryReader {
-         let frame = $0.frame(in: .global)
-         Image(systemName: "flag.circle.fill")
-             .foregroundStyle(.orange)
-             .animation(.snappy, body: { content in
-                 content.scaleEffect(isActive ? 1.3 : 1)
-             })
-             .onChange(of: isActive) { oldValue, newValue in
-                 let position = CGPoint(x: frame.midX, y: frame.midY)
-                 if let coordinate = proxy.convert(position, from: .global) {
-                     self.coordinate = coordinate
-                     translation = .zero
-                 }
-             }
-     }
-     .offset(translation)
-     .gesture(
-         LongPressGesture(minimumDuration: 0.10) // så att man ej råkar ta i den
-             .onEnded { isActive = $0 }
-             .simultaneously(with: DragGesture(minimumDistance: 0)
-                 .onChanged { value in
-                     if isActive { translation = value.translation }
-                 }
-                 .onEnded { value in
-                     if isActive { isActive = false }
-                 }
-             )
-     )
- }
- // Tee
- Annotation("1", coordinate: coordinate2) {
-     GeometryReader {
-         let frame = $0.frame(in: .global)
-         Image(systemName: "t.circle.fill")
-             .foregroundStyle(.yellow)
-             .animation(.snappy, body: { content in
-                 content.scaleEffect(isActive ? 1.3 : 1)
-             })
-             .onChange(of: isActive) { oldValue, newValue in
-                 let position = CGPoint(x: frame.midX, y: frame.midY)
-                 if let coordinate = proxy.convert(position, from: .global) {
-                     self.coordinate2 = coordinate
-                     translation2 = .zero
-                 }
-             }
-     }
-     .offset(translation2)
-     .gesture(
-         LongPressGesture(minimumDuration: 0.10) // så att man ej råkar ta i den
-             .onEnded { isActive = $0 }
-             .simultaneously(with: DragGesture(minimumDistance: 0)
-                 .onChanged { value in
-                     if isActive { translation2 = value.translation }
-                 }
-                 .onEnded { value in
-                     if isActive { isActive = false }
-                 }
-             )
-     )
- }
- */
