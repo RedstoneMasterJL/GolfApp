b7e4a2ade11cbc5ee4fc95702332a9e131d58e9e
Jonathan Linder (M2 mac) <jonathan.linder08@icloud.com>
2024-06-26 11:19:45 +0200

    markerview i separat fil

---
+++ GolfApp/MarkerView.swift
@@ -0,0 +1,57 @@
+//
+//  MarkerView.swift
+//  GolfApp
+//
+//  Created by Jonathan Linder on 2024-06-26.
+//
+
+import SwiftUI
+import MapKit
+
+enum MarkerType {
+    case green, tee
+}
+
+struct MarkerView: View {
+    var proxy: MapProxy
+    
+    let type: MarkerType
+    @State private var translation: CGSize = .zero
+    @State private var isActive: Bool = false
+    @Binding var coordinate: CLLocationCoordinate2D
+    
+    var body: some View {
+        GeometryReader {
+            let frame = $0.frame(in: .global)
+            
+            Image(systemName: type == .green ? "flag.circle.fill" : "t.circle.fill")
+                .foregroundStyle(type == .green ? .orange : .yellow)
+                .animation(.snappy, body: { content in
+                    content.scaleEffect(isActive ? 1.3 : 1)
+                })
+                .frame(width: frame.width, height: frame.height)
+                .onChange(of: isActive, initial: false) { oldValue, newValue in
+                    let position = CGPoint(x: frame.midX, y: frame.midY)
+                    if let coordinate = proxy.convert(position, from: .global) {
+                        self.coordinate = coordinate
+                        translation = .zero
+                    }
+                }
+        }
+        .frame(width: 70, height: 70)
+        .contentShape(.rect)
+        .offset(translation)
+        .gesture(
+            LongPressGesture(minimumDuration: 0.10) // så att man ej råkar ta i den
+                .onEnded { isActive = $0 }
+                .simultaneously(with: DragGesture(minimumDistance: 0)
+                    .onChanged { value in
+                        if isActive { translation = value.translation }
+                    }
+                    .onEnded { value in
+                        if isActive { isActive = false }
+                    }
+                )
+        )
+    }
+}
